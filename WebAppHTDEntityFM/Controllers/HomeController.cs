125d5fd5ad5fe8d20462fc5178ee7feaddadba03
SravaniBankapalli57 <114211950+SravaniBankapalli57@users.noreply.github.com>
2022-12-15 15:43:37 +0530

    Update HomeController.cs

--- WebAppHTDEntityFM/Controllers/HomeController.cs
+++ WebAppHTDEntityFM/Controllers/HomeController.cs
@@ -28,5 +28,9 @@ namespace WebAppHTDEntityFM.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult Privacy1()
+        {
+            return View();
+        } 
     }
 }
